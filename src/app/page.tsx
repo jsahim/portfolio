d9c0085accad41dee1a824291a334ffbfe700123
Jeff Sahim <jeffreysahim@gmail.com>
2025-02-19 10:34:39 -0500

    Update data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 'use client'
 
-import { useState } from "react";
+import React, { useState } from "react";
 
-export default function Home() {
-  
+interface HomeProps {
+  siteType: string;
+}
+
+export default function Home({ siteType }: HomeProps) {
   const [introReady, setIntro] = useState<boolean>(false);
   const [nameReady, setName] = useState<boolean>(false);
   const [endReady, setEnd] = useState<boolean>(false);
   const [orangeReady, setOrange] = useState<boolean>(false);
   
+  console.log(siteType)
+
   setTimeout(() => {
     setIntro(true)
   }, 100);
@@ -25,13 +30,21 @@ export default function Home() {
     setOrange(true)
   }, 1500);
 
+  // let position
+  // if(siteType === "ux"){
+  //   position = "UX Developer"
+  // } else if(siteType === "prod"){
+  //   position = "Product Developer"
+  // } else {
+  //   position = "Software Developer"
+  // }
 
   return (
       <div className="w-[85%] h-full text-center"> 
         <h1 className="font-[Fugaz] text-[#304962] mt-5">
           { introReady ? <><span className="relative z-[-1] animate-refine-slidein text-[5vw]">Hi there, I&apos;m</span><br/></> : null }
           { nameReady ? <><span className={`relative z-[-1] animate-refine-slidein text-[15vw] leading-relaxed ${orangeReady ? "text-[#e98522] duration-700": ""}`}>Jeff Sahim</span><br/></> : null }
-          { endReady ? <span className="relative z-[-1] animate-refine-slidein text-[5vw]">Product Developer</span> : null }
+          { endReady ? <span className="relative z-[-1] animate-refine-slidein text-[5vw]">{siteType}</span> : null }
         </h1>
       </div>
   )
