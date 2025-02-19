9f15e488d302a64cedf15dba48c32e4fd6df8b34
Jeff Sahim <jeffreysahim@gmail.com>
2025-02-19 13:53:06 -0500

    Update data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,29 +2,40 @@
 
 import { Montserrat } from 'next/font/google'
 import React, { useState, useEffect } from "react";
-import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation'
 import Navigation from "./_components/Navigation"
 import "./globals.css"
+
 
 const montserrat = Montserrat({
   subsets: ['latin'],
 })
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
+  const [siteType, setType] = useState<string>("");
   const searchParams = useSearchParams()
-  const [searchState, getState] = useState("")
-  const search = searchParams.get('type') || ""
-  
   
   useEffect(() => {
-    getState(search || "")
-      React.Children.map(children, (child) => {
-        if (React.isValidElement(child)) {
-          (child as React.ReactElement<any>).props.template.props.type = search
-        }
-      })
-    }, []);
+    const search = searchParams.get('type')
+    
+    if(!search || search === "dev" ){
+      setType("developer")
+    }else if(search === "prod"){
+      setType(search || "product")
+    } else if(search === "ux") {
+      setType("ux")
+    }
+    addChild()
+  }, []);
+
+  
+  const addChild = () => {
+    React.Children.map(children, (child) => {
+      if (React.isValidElement(child)) {
+        (child as React.ReactElement<any>).props.template.props.type = siteType
+      }
+    })
+  }
 
   return (
     <html lang="en">
@@ -33,12 +44,9 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
         <link rel="icon" type="image/x-icon" href="/images/mini-logo.png"></link>
       </head>
         <body>
-          <Navigation type={searchState}/>
-          
+          <Navigation type={siteType} />
           <main className='pt-28 flex flex-col items-center h-[100vh]'>
-            <Suspense fallback={<div>Loading...</div>}>
-              {children}
-            </Suspense>
+            {children}
           </main>
         </body>
       </html>
