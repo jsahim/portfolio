404c7bee7ad8f35a1427abb75032a6ff839ce038
Jeff Sahim <jeffreysahim@gmail.com>
2025-02-23 16:04:43 -0500

    Update style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,9 @@ export default function Home() {
 
     const siteType = allSearchParams.type
     const pageOrigin = allSearchParams.origin
+    if(pageOrigin){
+      setOrigin(pageOrigin)
+    }
 
     if(siteType === "u"){
       setSite("UX")
@@ -24,33 +27,32 @@ export default function Home() {
     } else {
       setSite("DEV")
     }
+
+    if(!origin){
+      setIntro(true)
+      setName(true)
+      setEnd(true)
+      setOrange(true)
+    } else {    
+      setTimeout(() => {
+        setIntro(true)
+      }, 100);
     
-    setOrigin(pageOrigin)
+      setTimeout(() => {
+        setName(true)
+      }, 200);
+      
+      setTimeout(() => {
+        setEnd(true)
+      }, 400);
+    
+      setTimeout(() => {
+        setOrange(true)
+      }, 1500);
+    }
 
   }, []);
   
-  if(origin){
-    setIntro(true)
-    setName(true)
-    setEnd(true)
-    setOrange(true)
-  } else {    
-    setTimeout(() => {
-      setIntro(true)
-    }, 100);
-  
-    setTimeout(() => {
-      setName(true)
-    }, 200);
-    
-    setTimeout(() => {
-      setEnd(true)
-    }, 400);
-  
-    setTimeout(() => {
-      setOrange(true)
-    }, 1500);
-  }
 
   let position
   if(site === "UX" ){
