9ca53f255ecdd62d0650619ca195a14c5cfd3967
Jeff Sahim <jeffreysahim@gmail.com>
2025-02-23 17:36:33 -0500

    Update style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,12 @@ export default function Home() {
   const [nameReady, setName] = useState<boolean>(false);
   const [endReady, setEnd] = useState<boolean>(false);
   const [orangeReady, setOrange] = useState<boolean>(false);
+  const [animation, setAnim] = useState<boolean>(true);
 
   useEffect(() => {
 
     const siteType = allSearchParams.type
+    const origType = allSearchParams.origin
 
     if(siteType === "u"){
       setSite("UX")
@@ -23,21 +25,29 @@ export default function Home() {
       setSite("DEV")
     }
 
-    setTimeout(() => {
-      setIntro(true)
-    }, 100);
-  
-    setTimeout(() => {
-      setName(true)
-    }, 200);
-    
-    setTimeout(() => {
-      setEnd(true)
-    }, 400);
-  
-    setTimeout(() => {
-      setOrange(true)
-    }, 1500);
+    if(origType){
+        setIntro(true)
+        setEnd(true)
+        setName(true)
+        setOrange(true)
+        setAnim(false)
+      } else {
+        setTimeout(() => {
+          setIntro(true)
+        }, 100);
+        
+        setTimeout(() => {
+          setName(true)
+        }, 200);
+        
+        setTimeout(() => {
+          setEnd(true)
+        }, 400);
+      
+        setTimeout(() => {
+          setOrange(true)
+        }, 1500);
+    }
 
   }, []);
 
@@ -54,9 +64,9 @@ export default function Home() {
   return (
       <div className="w-[85%] h-full text-center"> 
         <h1 className="font-fugaz text-[#304962] my-3">
-          { introReady ? <><span className={`relative z-[-1] animate-refine-slidein text-[5vw]`}>Hi there, I&apos;m</span><br/></> : null }
-          { nameReady ? <><span className={`relative z-[-1] animate-refine-slidein leading-relaxed ${orangeReady ? "text-[#e98522] duration-700 text-[15vw]": "text-[9vw]"}`}>Jeff Sahim</span><br/></> : null }
-          { endReady ? <span className="relative z-[-1] animate-refine-slidein text-[5vw]">{position}</span> : null }
+          { introReady ? <><span className={`relative z-[-1] text-[5vw] ${animation && "animate-refine-slidein"}`}>Hi there, I&apos;m</span><br/></> : null }
+          { nameReady ? <><span className={`relative z-[-1] leading-relaxed ${animation && "animate-refine-slidein"} ${orangeReady ? "text-[#e98522] duration-700 text-[15vw]": "text-[9vw]"}`}>Jeff Sahim</span><br/></> : null }
+          { endReady ? <span className={`relative z-[-1] text-[5vw] ${animation && "animate-refine-slidein"}`}>{position}</span> : null }
         </h1>
       </div>
   )
