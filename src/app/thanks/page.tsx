4a9993cf7a3327b37a815fdac0e1dfd0e4495e44
Jeff Sahim <jeffreysahim@gmail.com>
2025-02-24 22:47:17 -0500

    Update style

--- src/app/thanks/page.tsx
+++ src/app/thanks/page.tsx
@@ -1,10 +1,9 @@
 
 export default function ThankYou() {
 
-
   return (
     <div className="w-[85%] flex flex-col justify-center items-center h-[50%]"> 
-      <h1 className="text-6xl text-center py-6 font-medium" >Thanks for your message!</h1>
+      <h1 className="text-6xl text-center py-6 font-medium text-[#304962]">Thanks for your message!</h1>
       <p className="text-4xl text-center">I will respond as soon as possible.</p>
     </div>
   )
