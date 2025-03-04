d3d7cdeda5d94f8e53b33e285ee53391f83b43ec
Jeff Sahim <jeffreysahim@gmail.com>
2025-03-04 18:02:42 -0500

    Update style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
   }else if(site === "PROD"){
     position = "Product Developer"
   } else {
-    position = "Software Developer"
+    position = "Frontend Developer"
   }
 
   return (
