5d2eca6a9c9955d1ec25d775a2aa795d30cd72cb
Jeff Sahim <jeffreysahim@gmail.com>
2025-03-12 10:16:06 -0400

    Update style

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -12,7 +12,7 @@ export default function Projects() {
       <h2 className="text-3xl p-2 py-4 text-center font-semibold tracking-wide w-[100%] text-[#304962] z-1 rounded-[20px] bg-gray-50 mb-2 shadow-md">Software Projects</h2>
       <div className="flex flex-col w-[100%] h-full justify-center gap-5 bg-gray-50 rounded-[50px] shadow-md p-5 rounded-t-lg">
         
-        <div className="flex md:flex-row flex-col md:h-96 h-[auto] w-[100%] justify-between items-center">
+        <div className="flex md:flex-row flex-col md:h-96 h-[auto] w-[100%] justify-evenly items-center">
           <Image className="max-w-[500px] w-[100%] md:w-[50%] p-2 md:p-5" height={0} width={500} src="/images/Xpresso.png" alt="Xpresso Coffee" />
           <div className="flex flex-col w-[100%] md:w-[50%] p-3 md:p-5">
             <h3 className="text-2xl font-semibold text-left mb-3">Xpresso Coffee</h3>
@@ -27,7 +27,7 @@ export default function Projects() {
           </div>
         </div>
         
-        <div className="flex md:flex-row flex-col md:h-96 h-[auto] w-[100%] justify-between items-center" >
+        <div className="flex md:flex-row flex-col md:h-96 h-[auto] w-[100%] justify-evenly items-center" >
           <Image className="max-w-[500px] w-[100%] md:w-[50%] p-2 md:p-5" height={0} width={500} src="/images/Rancid.png" alt="Rancid Tomatillos" />
           <div className="flex flex-col w-[100%] md:w-[50%] p-3 md:p-5">
             <h3 className="text-2xl font-semibold text-left mb-3">Rancid Tomatillos</h3>
@@ -42,7 +42,7 @@ export default function Projects() {
           </div>
         </div>
 
-        <div className="flex md:flex-row flex-col md:h-96 h-[auto] w-[100%] justify-between items-center" >
+        <div className="flex md:flex-row flex-col md:h-96 h-[auto] w-[100%] justify-evenly items-center" >
           <Image className="max-w-[500px] w-[100%] md:w-[50%] p-2 md:p-5" height={0} width={500} src="/images/Hogwarts.png" alt="Battle of Hogwarts" />
           <div className="flex flex-col w-[100%] md:w-[50%] p-3 md:p-5">
             <h3 className="text-2xl font-semibold text-left mb-3">Battle of Hogwarts</h3>
