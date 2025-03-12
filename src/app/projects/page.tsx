f40f312249f6d8244cf2e1d00f4e0ccdbbd7562f
Jeff Sahim <jeffreysahim@gmail.com>
2025-03-12 01:24:29 -0400

    Update style

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -15,7 +15,7 @@ export default function Projects() {
         <div className="flex md:flex-row flex-col md:h-96 h-[auto] w-[100%] justify-between items-center">
           <Image className="max-w-[500px] w-[100%] md:w-[50%]" height={0} width={500} src="/images/Xpresso.png" alt="Xpresso Coffee" />
           <div className="flex flex-col w-[100%] md:w-[50%] p-3 md:p-5">
-            <h3 className="text-xl md:text-2xl font-semibold text-left mb-3">Xpresso Coffee</h3>
+            <h3 className="text-2xl font-semibold text-left mb-3">Xpresso Coffee</h3>
             <p className="text-base text-left pb-3">Order a coffee through Xpresso and have it delivered by drone!</p>
             <div className="my-0 flex flex-wrap justify-start *:px-3 *:py-1 *:m-1 *:text-white *:text-[12px] *:bg-[#304962] *:rounded-lg *:font-semibold">
               <span>React</span><span>JavaScript</span><span>HTML</span><span>CSS</span><span>React Router</span><span>Cypress</span><span>Git</span><span>Fetch API</span><span>Webpack</span><span>Photoshop</span>
@@ -30,7 +30,7 @@ export default function Projects() {
         <div className="flex md:flex-row flex-col md:h-96 h-[auto] w-[100%] justify-between items-center" >
           <Image className="max-w-[500px] w-[100%] md:w-[50%]" height={0} width={500} src="/images/Rancid.png" alt="Rancid Tomatillos" />
           <div className="flex flex-col w-[100%] md:w-[50%] p-3 md:p-5">
-            <h3 className="text-xl md:text-2xl font-semibold text-left mb-3">Rancid Tomatillos</h3>
+            <h3 className="text-2xl font-semibold text-left mb-3">Rancid Tomatillos</h3>
             <p className="text-base text-left pb-3">Can&apos;t decide on a movie to watch? Rancid Tomatillos has you covered.</p>
             <div className="my-0 flex flex-wrap justify-start *:px-3 *:py-1 *:m-1 *:text-white *:text-[12px] *:bg-[#304962] *:rounded-lg *:font-semibold">
               <span>React</span><span>JavaScript</span><span>HTML</span><span>CSS</span><span>React Router</span><span>Cypress</span><span>Git</span><span>Fetch API</span><span>Webpack</span><span>Photoshop</span>
@@ -45,7 +45,7 @@ export default function Projects() {
         <div className="flex md:flex-row flex-col md:h-96 h-[auto] w-[100%] justify-between items-center" >
           <Image className="max-w-[500px] w-[100%] md:w-[50%]" height={0} width={500} src="/images/Hogwarts.png" alt="Battle of Hogwarts" />
           <div className="flex flex-col w-[100%] md:w-[50%] p-3 md:p-5">
-            <h3 className="text-xl md:text-2xl font-semibold text-left mb-3">Battle of Hogwarts</h3>
+            <h3 className="text-2xl font-semibold text-left mb-3">Battle of Hogwarts</h3>
             <p className="text-base text-left pb-3">Battle of Hogwarts is a magical take on the classic game, Rock Paper Scissors.</p>
             <div className="my-0 flex flex-wrap justify-start *:px-3 *:py-1 *:m-1 *:text-white *:text-[12px] *:bg-[#304962] *:rounded-lg *:font-semibold">
               <span>JavaScript</span>
