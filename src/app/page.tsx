8d935638594fa4881f85f8d0cf748e090f30843a
Jeff Sahim <jeffreysahim@gmail.com>
2025-04-07 15:11:33 -0400

    Add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
   }else if(site === "PROD"){
     position = "Product Developer"
   } else {
-    position = "Frontend Developer"
+    position = "Dev/UX/Product"
   }
 
   return (
