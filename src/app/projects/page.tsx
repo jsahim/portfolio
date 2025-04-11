cd4e0da6a917ba4a9e9bdd3a695b6e709f51ea21
Jeff Sahim <jeffreysahim@gmail.com>
2025-04-11 16:04:58 -0400

    More style

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,14 +1,50 @@
+'use client'
+
+import { useState } from "react";
 import Image from "next/image"
 import { FiExternalLink } from "react-icons/fi";
 import { FaGithub } from "react-icons/fa";
 
 
 export default function Projects() {
+  
+  const [subcat, setSubcat] = useState("Wireframe")
+
+  const onOptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSubcat(e.target.value)
+  }
 
   return (
     <div className="w-[85%] pb-16 flex flex-col justify-center items-center">
       <h1 className="font-fugaz text-6xl text-[#e98522] text-center m-5 mb-16">Projects</h1>
       
+      <h2 id="uxProjects" className="text-3xl p-2 py-4 text-center font-semibold tracking-wide w-[100%] text-[#304962] z-1 rounded-[20px] bg-gray-50 mb-2 shadow-md">UX Development</h2>
+      
+
+      <div className="flex flex-col w-[100%] h-full justify-center gap-5 bg-gray-50 rounded-[50px] shadow-md p-5 rounded-t-lg mb-14">
+        <div className="flex w-[100%] justify-between">
+          <button className="w-[50%] h-[50px] flex justify-center content-center active:bg-violet-700">
+            <input className="w-[100%] opacity-0" type="radio" value="Wireframe" id="wireframe" checked={subcat === "Wireframe"} onChange={onOptionChange} />
+            <h3 className="absolute pointer-events-none">Wireframe</h3>
+          </button>
+          <button className="w-[50%] h-[50px] flex justify-center content-center active:bg-red-900">
+            <input className="w-[100%] opacity-0" type="radio" value="Website" id="website" checked={subcat === "Website"} onChange={onOptionChange} />
+            <h3 className="absolute pointer-events-none">Website</h3>
+          </button>
+
+        </div>
+        
+        { subcat === "Wireframe" ? 
+          <div className="flex flex-col h-[auto] w-[100%] justify-evenly items-center">
+            <Image className="w-[100%] p-2 md:p-5" height={0} width={1000} src="/images/menu.png" alt="Menu" />
+          </div> 
+          :<div className="flex flex-col h-[auto] w-[100%] justify-evenly items-center">
+              <h1>LIVE WEBSITE</h1>
+          </div> 
+        }
+        
+      </div>
+
       <h2 id="softProjects" className="text-3xl p-2 py-4 text-center font-semibold tracking-wide w-[100%] text-[#304962] z-1 rounded-[20px] bg-gray-50 mb-2 shadow-md">Software Projects</h2>
       <div className="flex flex-col w-[100%] h-full justify-center gap-5 bg-gray-50 rounded-[50px] shadow-md p-5 rounded-t-lg">
         
