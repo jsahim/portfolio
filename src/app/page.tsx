65d600c316c9938542137aaad4757b4fa010c783
Jeff Sahim <jeffreysahim@gmail.com>
2025-04-22 18:08:39 -0400

    Add image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
   }else if(site === "PROD"){
     position = "Product Developer"
   } else {
-    position = "Dev · Product · UX"
+    position = "Technical Product Manager"
   }
 
   return (
