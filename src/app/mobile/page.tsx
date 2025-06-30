71c145d840e5cd9c58b2154988ed959290827591
Jeff Sahim <jeffreysahim@gmail.com>
2025-06-30 10:38:26 -0400

    Add mobile page

---
+++ src/app/mobile/page.tsx
@@ -0,0 +1,172 @@
+import Image from "next/image"
+
+
+export default function Mobile() {
+  return (
+    <div className="w-[85%] pb-16 flex flex-col justify-center items-center">
+      <h1 className="font-fugaz text-6xl text-[#e98522] text-center m-5 mb-16">Mobile Solutions</h1>
+      
+      <h2 id="uxProjects" className="text-3xl p-2 py-4 text-center font-semibold tracking-wide w-[100%] text-[#304962] z-1 rounded-[20px] bg-gray-50 mb-2 shadow-md">Digital Guest Directory</h2>
+      <div className="flex flex-wrap w-[100%] h-full justify-center bg-gray-50 rounded-[50px] shadow-md p-5 rounded-t-lg mb-14">
+
+          <div className="flex flex-col h-[auto] justify-evenly items-start p-5">
+            <div className="w-[100%] flex flex-col justify-evenly items-center">
+              <h2 className="text-2xl font-semibold text-left mb-3">Hyatt Studios</h2>
+              <div className="flex relative flex-col scale-75 origin-top justify-start content-center w-[370px] h-[670px]">
+                <iframe className="w-[350px] h-[585px] absolute translate-x-[0.6rem] translate-y-24" src="https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/dist/index.html?template=hyatt-studios&uid=428835&homepage=https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/Welcome_Page_HyattStudios.html" title="Menu App" />
+                <Image className="w-[370px] h-auto absolute pointer-events-none" height={0} width={1000} src="/images/frame.png" alt="Frame" />
+              </div>
+
+            </div>
+          </div> 
+
+          <div className="flex flex-col h-[auto] justify-evenly items-start p-5">
+            <div className="w-[100%] flex flex-col justify-evenly items-center">
+              <h2 className="text-2xl font-semibold text-left mb-3">Hyatt House</h2>              
+              <div className="flex relative flex-col scale-75 origin-top justify-start content-center w-[370px] h-[670px]">
+                <iframe className="w-[350px] h-[585px] absolute translate-x-[0.6rem] translate-y-24" src="https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/dist/index.html?template=me-and-all&uid=339165&homepage=https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/Welcome_Page_HyattHouse.html" title="Menu App" />
+                <Image className="w-[370px] h-auto absolute pointer-events-none" height={0} width={1000} src="/images/frame.png" alt="Frame" />
+              </div>
+
+            </div>
+          </div> 
+
+          <div className="flex flex-col h-[auto] justify-evenly items-start p-5">
+            <div className="w-[100%] flex flex-col justify-evenly items-center">
+              <h2 className="text-2xl font-semibold text-left mb-3">HYATT</h2>              
+              <div className="flex relative flex-col scale-75 origin-top justify-start content-center w-[370px] h-[670px]">
+                <iframe className="w-[350px] h-[585px] absolute translate-x-[0.6rem] translate-y-24" src="https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/dist/index.html?template=hyatt-brand&uid=393770&homepage=https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/Welcome_Page_HyattBrand.html" title="Menu App" />
+                <Image className="w-[370px] h-auto absolute pointer-events-none" height={0} width={1000} src="/images/frame.png" alt="Frame" />
+              </div>
+
+            </div>
+          </div> 
+
+          <div className="flex flex-col h-[auto] justify-evenly items-start p-5">
+            <div className="w-[100%] flex flex-col justify-evenly items-center">
+              <h2 className="text-2xl font-semibold text-left mb-3">Park Hyatt</h2>              
+              <div className="flex relative flex-col scale-75 origin-top justify-start content-center w-[370px] h-[670px]">
+                <iframe className="w-[350px] h-[585px] absolute translate-x-[0.6rem] translate-y-24" src="https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/dist/index.html?template=park-hyatt&uid=368146&homepage=https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/Welcome_Page_ParkHyatt.html" title="Menu App" />
+                <Image className="w-[370px] h-auto absolute pointer-events-none" height={0} width={1000} src="/images/frame.png" alt="Frame" />
+              </div>
+
+            </div>
+          </div> 
+
+          <div className="flex flex-col h-[auto] justify-evenly items-start p-5">
+            <div className="w-[100%] flex flex-col justify-evenly items-center">
+              <h2 className="text-2xl font-semibold text-left mb-3">Alila</h2>              
+              <div className="flex relative flex-col scale-75 origin-top justify-start content-center w-[370px] h-[670px]">
+                <iframe className="w-[350px] h-[585px] absolute translate-x-[0.6rem] translate-y-24" src="https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/dist/index.html?template=alila&uid=352758&homepage=https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/Welcome_Page_Alila.html" title="Menu App" />
+                <Image className="w-[370px] h-auto absolute pointer-events-none" height={0} width={1000} src="/images/frame.png" alt="Frame" />
+              </div>
+
+            </div>
+          </div> 
+
+          <div className="flex flex-col h-[auto] justify-evenly items-start p-5">
+            <div className="w-[100%] flex flex-col justify-evenly items-center">
+              <h2 className="text-2xl font-semibold text-left mb-3">Dream Hotels</h2>              
+              <div className="flex relative flex-col scale-75 origin-top justify-start content-center w-[370px] h-[670px]">
+                <iframe className="w-[350px] h-[585px] absolute translate-x-[0.6rem] translate-y-24" src="https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/dist/index.html?template=dream&uid=347869&homepage=https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/Welcome_Page_Dream.html" title="Menu App" />
+                <Image className="w-[370px] h-auto absolute pointer-events-none" height={0} width={1000} src="/images/frame.png" alt="Frame" />
+              </div>
+
+            </div>
+          </div> 
+
+          <div className="flex flex-col h-[auto] justify-evenly items-start p-5">
+            <div className="w-[100%] flex flex-col justify-evenly items-center">
+              <h2 className="text-2xl font-semibold text-left mb-3">Grand Hyatt</h2>              
+              <div className="flex relative flex-col scale-75 origin-top justify-start content-center w-[370px] h-[670px]">
+                <iframe className="w-[350px] h-[585px] absolute translate-x-[0.6rem] translate-y-24" src="https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/dist/index.html?template=grand-hyatt&uid=329275&homepage=https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/Welcome_Page_GrandHyatt.html" title="Menu App" />
+                <Image className="w-[370px] h-auto absolute pointer-events-none" height={0} width={1000} src="/images/frame.png" alt="Frame" />
+              </div>
+
+            </div>
+          </div> 
+
+          <div className="flex flex-col h-[auto] justify-evenly items-start p-5">
+            <div className="w-[100%] flex flex-col justify-evenly items-center">
+              <h2 className="text-2xl font-semibold text-left mb-3">Andaz</h2>              
+              <div className="flex relative flex-col scale-75 origin-top justify-start content-center w-[370px] h-[670px]">
+                <iframe className="w-[350px] h-[585px] absolute translate-x-[0.6rem] translate-y-24" src="https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/dist/index.html?template=andaz&uid=329186&homepage=https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/Welcome_Page_Andaz.html" title="Menu App" />
+                <Image className="w-[370px] h-auto absolute pointer-events-none" height={0} width={1000} src="/images/frame.png" alt="Frame" />
+              </div>
+
+            </div>
+          </div> 
+
+          <div className="flex flex-col h-[auto] justify-evenly items-start p-5">
+            <div className="w-[100%] flex flex-col justify-evenly items-center">
+              <h2 className="text-2xl font-semibold text-left mb-3">Caption by Hyatt</h2>              
+              <div className="flex relative flex-col scale-75 origin-top justify-start content-center w-[370px] h-[670px]">
+                <iframe className="w-[350px] h-[585px] absolute translate-x-[0.6rem] translate-y-24" src="https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/dist/index.html?template=caption&uid=322897&homepage=https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/Welcome_Page_CaptionHyatt.html" title="Menu App" />
+                <Image className="w-[370px] h-auto absolute pointer-events-none" height={0} width={1000} src="/images/frame.png" alt="Frame" />
+              </div>
+
+            </div>
+          </div> 
+
+          <div className="flex flex-col h-[auto] justify-evenly items-start p-5">
+            <div className="w-[100%] flex flex-col justify-evenly items-center">
+              <h2 className="text-2xl font-semibold text-left mb-3">Thompson Hotels</h2>              
+              <div className="flex relative flex-col scale-75 origin-top justify-start content-center w-[370px] h-[670px]">
+                <iframe className="w-[350px] h-[585px] absolute translate-x-[0.6rem] translate-y-24" src="https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/dist/index.html?template=thompson&uid=275980&homepage=https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/Welcome_Page_Thompson.html" title="Menu App" />
+                <Image className="w-[370px] h-auto absolute pointer-events-none" height={0} width={1000} src="/images/frame.png" alt="Frame" />
+              </div>
+
+            </div>
+          </div> 
+
+          <div className="flex flex-col h-[auto] justify-evenly items-start p-5">
+            <div className="w-[100%] flex flex-col justify-evenly items-center">
+              <h2 className="text-2xl font-semibold text-left mb-3">Hyatt Centric</h2>              
+              <div className="flex relative flex-col scale-75 origin-top justify-start content-center w-[370px] h-[670px]">
+                <iframe className="w-[350px] h-[585px] absolute translate-x-[0.6rem] translate-y-24" src="https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/dist/index.html?template=hyatt-centric&uid=275998&homepage=https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/Welcome_Page_HyattCentric.html" title="Menu App" />
+                <Image className="w-[370px] h-auto absolute pointer-events-none" height={0} width={1000} src="/images/frame.png" alt="Frame" />
+              </div>
+
+            </div>
+          </div> 
+
+          <div className="flex flex-col h-[auto] justify-evenly items-start p-5">
+            <div className="w-[100%] flex flex-col justify-evenly items-center">
+              <h2 className="text-2xl font-semibold text-left mb-3">Hyatt Regency</h2>              
+              <div className="flex relative flex-col scale-75 origin-top justify-start content-center w-[370px] h-[670px]">
+                <iframe className="w-[350px] h-[585px] absolute translate-x-[0.6rem] translate-y-24" src="https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/dist/index.html?template=hyatt-regency&uid=379634&homepage=https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/Welcome_Page_HyattRegency.html" title="Menu App" />
+                <Image className="w-[370px] h-auto absolute pointer-events-none" height={0} width={1000} src="/images/frame.png" alt="Frame" />
+              </div>
+
+            </div>
+          </div> 
+
+                    <div className="flex flex-col h-[auto] justify-evenly items-start p-5">
+            <div className="w-[100%] flex flex-col justify-evenly items-center">
+              <h2 className="text-2xl font-semibold text-left mb-3">Hyatt Place</h2>              
+              <div className="flex relative flex-col scale-75 origin-top justify-start content-center w-[370px] h-[670px]">
+                <iframe className="w-[350px] h-[585px] absolute translate-x-[0.6rem] translate-y-24" src="https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/dist/index.html?template=hyatt-place&uid=255641&homepage=https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/Welcome_Page_HyattPlace.html" title="Menu App" />
+                <Image className="w-[370px] h-auto absolute pointer-events-none" height={0} width={1000} src="/images/frame.png" alt="Frame" />
+              </div>
+
+            </div>
+          </div> 
+
+                    <div className="flex flex-col h-[auto] justify-evenly items-start p-5">
+            <div className="w-[100%] flex flex-col justify-evenly items-center">
+              <h2 className="text-2xl font-semibold text-left mb-3">Inn at the Forks</h2>              
+              <div className="flex relative flex-col scale-75 origin-top justify-start content-center w-[370px] h-[670px]">
+                <iframe className="w-[350px] h-[585px] absolute translate-x-[0.6rem] translate-y-24" src="https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/dist/index.html?template=sparrow&uid=570003&homepage=https://plusonestorage.blob.core.windows.net/asset-storage/QRD/PROD/Home_Page_SparrowHotels.html" title="Menu App" />
+                <Image className="w-[370px] h-auto absolute pointer-events-none" height={0} width={1000} src="/images/frame.png" alt="Frame" />
+              </div>
+
+            </div>
+          </div> 
+          
+      </div>
+
+    
+
+    </div>
+  )
+}
