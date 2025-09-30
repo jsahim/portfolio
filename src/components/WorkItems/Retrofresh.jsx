7fe0c1c83cdd11267033caa5e384b8f5218af114
Jeff Sahim <jeffreysahim@gmail.com>
2025-09-30 13:55:20 -0400

    Add Work Item

--- src/components/WorkItems/Retrofresh.jsx
+++ src/components/WorkItems/Retrofresh.jsx
@@ -1,21 +1,21 @@
 const Retrofresh = () => {
 
   return (
-    <div className="text-black">
+    <div className="text-black border-4 border-red-600 bg-white rounded-lg p-10">
       
       <h3 className="font-bold text-xl">CONCESSION ORDERING APP</h3><br/><br/>
 
       <div className="space-y-4 grid md:grid-cols-2 gap-15 md:gap-8">
-        
+
         <div>
-          somehting
+          IMAGE
         </div>
         <div>
-          <p><strong>PROJECT OVERVIEW</strong><br/>This three-week project focused on improving the customer experience and operational efficiency of a local park's concession stand, which suffered from severe queue backups during peak hours, particularly at game halftimes and intermissions.</p>
+          <p><strong>PROJECT OVERVIEW</strong><br/><br/>This three-week project focused on improving the customer experience and operational efficiency of a local park's concession stand, which suffered from severe queue backups during peak hours, particularly at game halftimes and intermissions.</p>
           <p>Client: Concession Stand Manager at a local park</p>
           <p>Duration: 3 weeks</p>
           <p>My Role: Research, Wireframing & User Flows, Data Architecture, Software Development</p>
-          <p>Summary: Developed a mobile-first web application that allows patrons to browse the concession menu and place orders remotely. The application’s menu data is dynamically managed by an administrator using a simple Google Sheet, which is converted into a REST API to populate the app.</p><br/><br/>
+          <p>Summary: Developed a mobile-first web application that allows patrons to browse the concession menu and place orders remotely. The application's menu data is dynamically managed by an administrator using a simple Google Sheet, which is converted into a REST API to populate the app.</p><br/><br/>
         </div>
 
       </div>
@@ -25,11 +25,11 @@ const Retrofresh = () => {
         <div className="list-decimal list-inside space-y-4 grid md:grid-cols-2 gap-15 md:gap-8">
           
           <div>
-            <p>THE CHALLENGE: Reducing Wait Times and Indecision<br/>The primary problem was the frequent and lengthy line backups at the concession stand during peak event times. Patron feedback and initial observations revealed that these delays were significantly compounded by patron indecision when ordering at the window, leading to long transaction times and frustration. Patrons were missing portions of the games they came to see.</p>
+            <p><strong>THE CHALLENGE: REDUCING WAIT TIMES AND INDICISION</strong><br/><br/>The primary problem was the frequent and lengthy line backups at the concession stand during peak event times. Patron feedback and initial observations revealed that these delays were significantly compounded by patron indecision when ordering at the window, leading to long transaction times and frustration. Patrons were missing portions of the games they came to see.</p>
             <p>Key Issues Identified:<br/>Peak Congestion: Halftimes and intermissions saw massive influxes of customers.<br/>Menu Blindness: Patrons had no way to view the menu, prices, or options until they were physically at the window, leading to on-the-spot decision-making.<br/>Lost Time: Extended wait times resulted in patrons missing game action.</p><br/><br/>
           </div>
           <div>
-            <p>THE SOLUTION: A Mobile-First Pre-Ordering App<br/>The solution was to provide patrons with easy, remote access to the full menu and the ability to place orders before they even approached the stand, addressing indecision and queue length simultaneously.The solution was to provide patrons with easy, remote access to the full menu and the ability to place orders before they even approached the stand, addressing indecision and queue length simultaneously.</p>
+            <p><strong>THE SOLUTION: A MOBILE-FIRST PRE-ORDERING APP</strong><br/><br/>The solution was to provide patrons with easy, remote access to the full menu and the ability to place orders before they even approached the stand, addressing indecision and queue length simultaneously.The solution was to provide patrons with easy, remote access to the full menu and the ability to place orders before they even approached the stand, addressing indecision and queue length simultaneously.</p>
             <p>Strategy:<br/>Menu Visibility: Provide a digital, mobile-first menu accessible anywhere in the park.<br/>Order Ahead: Implement a feature for patrons to place and pay for orders directly through the app.<br/>Easy Access: Utilize strategically placed QR codes around the park (near seating areas, entrances) to provide instant access to the app menu.</p><br/><br/>
           </div>
 
@@ -37,7 +37,7 @@ const Retrofresh = () => {
 
       <hr /><br/><br/>
 
-      <p>PROCESS & EXECUTION
+      <p><strong>PROCESS & EXECUTION</strong><br/><br/>
 
         <ol className="list-decimal list-inside space-y-4 grid md:grid-cols-3 gap-15 md:gap-8">
           <li>Research & Analysis<br/>
@@ -60,11 +60,16 @@ const Retrofresh = () => {
       
       <hr /><br/><br/>
 
-      <p>RESULTS & IMPACT<br/>The implementation of the app immediately started to show positive effects on the concession stand operations.</p><br/><br/>
-            
-      <hr /><br/><br/>
-
-      <p>CONCLUSION<br/>This project demonstrated that a simple, focused application can solve a complex operational problem. By leveraging common, accessible tools (Google Sheets) for the backend and pairing them with a modern, mobile-first frontend (React), we delivered an impactful UX solution that benefits both the business (faster service, higher throughput) and the user (less waiting, more game time).</p>
+      <div className="list-decimal list-inside space-y-4 grid md:grid-cols-2 gap-15 md:gap-8">  
+        <div>
+          <p><strong>RESULTS & IMPACT</strong><br/><br/>The implementation of the app immediately started to show positive effects on the concession stand operations.</p> <br/><br/>
+          <p><strong>CONCLUSION</strong><br/><br/>This project demonstrated that a simple, focused application can solve a complex operational problem. By leveraging common, accessible tools (Google Sheets) for the backend and pairing them with a modern, mobile-first frontend (React), we delivered an impactful UX solution that benefits both the business (faster service, higher throughput) and the user (less waiting, more game time).</p>
+        </div>
+        <div>
+          IMAGE
+        </div>
+          
+      </div>
 
     </div>
   )
