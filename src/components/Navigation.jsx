3261a8cde156fbb9406add65f8d9da531eb2854b
Jeff Sahim <jeffreysahim@gmail.com>
2025-10-07 23:06:50 -0400

    Add Styles

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,7 +3,7 @@ const Navigation = ({currentSection, setCurrentSection, scrollToTargetDiv, workS
   return (
     <nav className="tracking-widest fixed flex lg:pt-3 lg:pb-auto pt-auto pb-2 justify-center items-center lg:top-0 lg:bottom-auto top-auto bottom-0 left-0 right-0 z-50 bg-none transition-transform duration-300 pointer-events-none">
 
-      <div className="max-w-6xl mr-0 bg-white/90 rounded-full backdrop-blur-md border-1 border-gray-200 lg:w-150 w-[90vw] shadow-md transition-transform duration-300 pointer-events-auto">
+      <div className="max-w-6xl mr-0 bg-white/90 rounded-xl backdrop-blur-md border-1 border-gray-200 lg:w-150 w-[90vw] shadow-md transition-transform duration-300 pointer-events-auto">
         <div className="flex justify-center items-center h-14">
           <div className="flex w-full justify-evenly items-center">
 
