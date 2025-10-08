975c0d435506c41a6e198f7218ad553d88b6fe00
Jeff Sahim <jeffreysahim@gmail.com>
2025-10-08 13:07:48 -0400

    Add Styles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,8 +12,10 @@ const Contact = ({contactSection, topSection, scrollToTargetDiv}) => {
           <h2 className="text-4xl sm:text-5xl font-bold text-[#f22b40] mb-6 text-left" style={{fontFamily: 'Shrikhand, cursive'}}>
             Let's Connect!
           </h2>
+          <p className="text-xl mb-2 text-gray-400">Connect with me to discuss any opportunities.</p>
           <div className="flex justify-start items-center">
             <div className="flex flex-col lg:flex-row justify-center w-fit items-start gap-2 lg:gap-10">
+
               <a className="hover:border-[#f22b40] pb-1 border-transparent border-b-2 font-lg flex w-fit group justify-start items-center text-xl font-semibold text-gray-300 hover:text-white transition-all duration-300" href="mailto:jeffreysahim@gmail.com">
                 <TbMailFilled className="w-6 h-6" />
                 <span className="ml-2">SEND ME AN EMAIL</span> 
@@ -26,7 +28,7 @@ const Contact = ({contactSection, topSection, scrollToTargetDiv}) => {
           </div>
         </div>
         <div className="flex justify-center lg:justify-end">
-          <button className="cursor-pointer z-5 w-20 h-20 lg:w-25 lg:h-25 mt-10 lg:mt-0 group rounded-full bg-[#f22b40] transition-all duration-300 flex justify-center items-center text-white font-bold" 
+          <button className="cursor-pointer z-5 w-20 h-20 lg:w-35 lg:h-35 mt-10 lg:mt-0 group rounded-full bg-[#f22b40] transition-all duration-300 flex justify-center items-center text-white font-bold" 
             onClick={()=> scrollToTargetDiv(topSection)}>
               <FaHandPointer className='move-icon stroke-2 w-8 h-8 lg:w-15 lg:h-15 transition-all duration-300'/> 
           </button>
