f67bb3e21309f81ab05a1c2532c9f142653fb24d
Jeff Sahim <jeffreysahim@gmail.com>
2025-10-08 11:50:57 -0400

    Add Styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
 
       <div className="max-w-6xl mx-auto">
 
-        <div className="mt-50 lg:mt-0 text-white tracking-widest font-light" style={{ fontFamily: 'Jost, sans-serif'}}>
+        <div className="mt-[30vh] lg:mt-0 text-white tracking-widest font-light" style={{ fontFamily: 'Jost, sans-serif'}}>
           <div className='text-[#f22b40] leading-15 lg:leading-38 tracking-normal text-[4rem] lg:text-[12rem] flex flex-col lg:flex-row justify-center items-center' style={{ fontFamily: 'Shrikhand, cursive' }}>
             <IconRotate />
             <span className="z-1 pl-0 lg:pl-10">Jeff<br/>Sahim</span>
