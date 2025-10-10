e7f0eb177d8c5a444f59b23668452cb1061280c3
Jeff Sahim <jeffreysahim@gmail.com>
2025-10-09 21:36:23 -0400

    Add Styles

--- src/components/WorkItems/SnackShack.jsx
+++ src/components/WorkItems/SnackShack.jsx
@@ -22,7 +22,7 @@ const SnackShack = () => {
       
       <div className="flex justify-between items-start sm:items-center">
         <h3 className="font-bold text-black text-xl">
-          THE SNACK SHACK<span className='text-gray-600 font-semibold'> ● CONCESSION ORDERING APP</span>
+          THE SNACK SHACK<span className='text-gray-600 font-semibold'> | CONCESSION ORDERING APP</span>
         </h3>
         <div className="flex flex-col justify-start">
           <button className="cursor-pointer z-5 w-12 h-12 group rounded-full bg-[#f22b40] transition-all duration-300 flex justify-center items-center text-white font-bold" 
@@ -56,7 +56,7 @@ const SnackShack = () => {
         <div className="space-y-4 grid md:grid-cols-2 gap-0 md:gap-8 lg:gap-15">
         
           <div>
-            <p className='py-2'><strong>THE CHALLENGE: <span className="text-gray-600">REDUCING WAIT TIMES AND INDICISION</span></strong></p>
+            <p className='py-2'><strong>THE CHALLENGE: <span className="text-gray-600 font-semibold">REDUCING WAIT TIMES AND INDICISION</span></strong></p>
             <p>The primary problem was the frequent and lengthy line backups at the concession stand during peak event times. Patron feedback and initial observations revealed that these delays were significantly compounded by patron indecision when ordering at the window, leading to long transaction times and frustration. Patrons were missing portions of the games they came to see.</p>
             <p className='py-2'><strong>Key Issues Identified</strong></p>
             <div>
@@ -69,7 +69,7 @@ const SnackShack = () => {
           <hr className='border border-gray-300 my-10 block sm:hidden'/>
 
           <div>
-            <p className='py-2'><strong>THE SOLUTION: <span className="text-gray-600">A MOBILE-FIRST PRE-ORDERING APP</span></strong></p>
+            <p className='py-2'><strong>THE SOLUTION: <span className="text-gray-600 font-semibold">A MOBILE-FIRST PRE-ORDERING APP</span></strong></p>
             <p>The solution was to provide patrons with easy, remote access to the full menu and the ability to place orders before they even approached the stand, addressing indecision and queue length simultaneously.The solution was to provide patrons with easy, remote access to the full menu and the ability to place orders before they even approached the stand, addressing indecision and queue length simultaneously.</p>
             <p className='py-2'><strong>Strategy</strong></p>
             <div>
@@ -92,7 +92,7 @@ const SnackShack = () => {
             </li>
             <div className='pl-5'>
               <p><strong>User Interviews:</strong> Conducted brief interviews with park patrons to confirm the pain points. The consensus was a desire to skip the line and the need for pre-order viewing of the menu.</p>
-              <p><strong>Flow Mapping:</strong> Mapped the existing inefficient patron flow <span className='font-semibold italic'>(Wait in line → View menu → Decide → Order/Pay → Wait for fulfillment)</span> and compared it to the proposed streamlined flow <span className='font-semibold italic'>(Scan QR → View/Order → Pickup at designated area)</span>.</p>
+              <p><strong>Flow Mapping:</strong> Mapped the existing inefficient patron flow <span className='font-semibold italic'>(Wait in line → View menu → Decide → Order/Pay → Wait for fulfillment)</span> and compared it to the proposed streamlined flow <span className='font-semibold italic'>(Scan QR → View/Order → Pay/Pickup at designated area)</span>.</p>
             </div>
             <li className='font-bold py-2'>
               Data Architecture & Administration
