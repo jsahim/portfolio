e1645c47cc2734c9bad75fdd8147d69648b0af7d
Jeff Sahim <jeffreysahim@gmail.com>
2025-11-10 12:08:35 -0500

    Update Styles

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -49,7 +49,7 @@ const Work = ({workSection}) => {
               <span className='bg-gray-50 px-2'>THE SNACK SHACK</span>
             </h3>
             <div className="border-4 h-full group-hover:border-[#f22b40] rounded-2xl transition-colors duration-300">
-              <p className='font-medium text-xl text-gray-700 text-center px-5 pt-5 pb-1'>Mobile Ordering for Concessions</p>
+              <p className='font-medium text-xl text-gray-700 text-center px-5 pt-5 pb-1'>Ordering for Concessions</p>
               <div className='relative h-[400px] w-full flex items-center justify-center group overflow-hidden mb-10 md:mb-0'>
                 <img 
                   className='absolute w-[170px] group-hover:w-[180px] pointer-events-none transition-all duration-500 ease-out z-10' 
